fed46c3e178dfd86553e9e8f0271a6c574155fa9
NandinhaDev <fevelasco14@gmail.com>
2024-02-10 01:05:41 +0000

    finished script.js and adapted UI to mobile

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const face = document.querySelector('.face')
 const calculator = document.querySelector('.calculator')
-
+const result = document.getElementById('result')
+const multiplicate = document.getElementById("multiplicateButton")
+const divide = document.getElementById("divideButton")
 
 face.addEventListener('click', startCalculator)
 calculator.addEventListener('click', endCalculator)
@@ -12,3 +14,26 @@ function endCalculator() {
     calculator.classList.remove("active")
 }
 
+function display(val) {
+    result.value += val  
+    
+    return val
+}
+
+function deleteNumber() {
+    const currentValue = result.value;  
+    if (currentValue.length > 0) {   
+         result.value = currentValue.slice(0, -1)
+        }
+}
+
+function clearScreen (){
+    result.value = ""
+}
+
+function solve() {
+    let calculation = result.value.replace(/x/g, '*').replace(/÷/g,'/')
+    let solution = eval(calculation)
+    result.value = solution
+    return solution 
+}
